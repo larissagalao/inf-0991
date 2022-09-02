f49000785c906b3b561a6f66a57df42242ff23f8
larissagalao <83936720+larissagalao@users.noreply.github.com>
2022-09-02 16:20:07 -0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace inf0991
+{
+    public class HelloWorld
+    {
+        private static void Main(string[] args){ 
+            Console.WriteLine("Hello");
+            string name;
+            name = Console.ReadLine();
+
+            if(name.Length > 0){
+                Console.WriteLine("Hello" + name);
+            }       
+        }
+    }
+}
+
